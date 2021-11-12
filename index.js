7d25bcc91e0e911d0de4be080f9489d9c4bbf755
strtanberahmed <tanber1111@gmail.com>
2021-11-12 20:26:16 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require("cors");
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gezhp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     // perform actions on the collection object
+//     client.close();
+// });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('strCarWorld')
+        const servicesCollection = database.collection('services');
+
+        // GET API
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        // POST API
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            console.log('str', service);
+
+            const result = await servicesCollection.insertOne(service);
+            res.json(result);
+
+            res.send('hitted')
+        });
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('hello bro from my help hand server');
+});
+
+app.listen(port, () => {
+    console.log('Listening to port', port);
+})
+
+// user: strcarworld1
+// pass: ixJ1VASDyampmvNp
